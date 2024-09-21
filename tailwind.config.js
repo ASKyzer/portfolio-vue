53dd5ff2ed10a7a583a42193e88a43cfec6013a2
Adrian Kyzer <adriankyzer@gmail.com>
2024-09-21 17:28:42 +0200

    create global custom colors and properties

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,21 @@
-// tailwind.config.js
-export default {
-  content: ['./src/**/*.{html,js,vue}'],
-
+module.exports = {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx,vue}', 
+  ],
   theme: {
     extend: {
-      letterSpacing: {
-        tight: '-0.2rem' // Equivalent to -2px at base font size
-      }
-    }
+      colors: {
+        background: 'var(--color-background)',
+        error: 'var(--color-error)',
+        primary: 'var(--color-primary)', 
+        secondary: 'var(--color-secondary)', 
+        tertiary: 'var(--color-tertiary)',
+        tertiaryLight: 'var(--color-tertiary-light)',
+        tertiaryLighter: 'var(--color-tertiary-lighter)',
+        tertiaryDark: 'var(--color-tertiary-dark)',
+        tertiaryDarker: 'var(--color-tertiary-darker)',
+      },
+    },
   },
-
-  plugins: []
-}
+  plugins: [],
+};
