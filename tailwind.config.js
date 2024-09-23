73b466ec35ba8e7cccb3de8a72bcaceeac398c7d
Adrian Kyzer <adriankyzer@gmail.com>
2024-09-24 00:47:46 +0200

    create loading component

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,25 @@
 module.exports = {
-  content: [
-    './src/**/*.{html,js,jsx,ts,tsx,vue}', 
-  ],
+  content: ['./src/**/*.{html,js,jsx,ts,tsx,vue}'],
   theme: {
     extend: {
       colors: {
         background: 'var(--color-background)',
         error: 'var(--color-error)',
-        primary: 'var(--color-primary)', 
-        secondary: 'var(--color-secondary)', 
+        primary: 'var(--color-primary)',
+        secondary: 'var(--color-secondary)',
         tertiary: 'var(--color-tertiary)',
         tertiaryLight: 'var(--color-tertiary-light)',
         tertiaryLighter: 'var(--color-tertiary-lighter)',
         tertiaryDark: 'var(--color-tertiary-dark)',
-        tertiaryDarker: 'var(--color-tertiary-darker)',
-      },
+        tertiaryDarker: 'var(--color-tertiary-darker)'
+      }
     },
     letterSpacing: {
       tight: '-0.2rem' // Equivalent to -2px at base font size
+    },
+    animation: {
+      pulse: 'pulse 1.5s ease-in-out infinite'
     }
   },
-  plugins: [],
+  plugins: []
 };
