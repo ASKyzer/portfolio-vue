bca5c9a4fdb077d8b4776ff493dc07e3283bf3dd
Adrian Kyzer <adriankyzer@gmail.com>
2024-09-23 13:41:45 +0200

    create a button component

---
+++ src/components/ButtonComponent.vue
@@ -0,0 +1,45 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  props: {
+    text: {
+      type: String,
+      default: ''
+    },
+    size: {
+      type: String as () => 'sm' | 'md' | 'lg',
+      default: 'md'
+    },
+    type: {
+      type: String as () => 'button' | 'reset' | 'submit',
+      default: 'button'
+    },
+    theme: {
+      type: String as () => 'primary' | 'secondary' | 'inverse',
+      default: 'primary'
+    }
+  },
+  setup(props) {
+    return props;
+  }
+});
+</script>
+
+<template>
+  <button :type="type" :class="['button-' + theme, size]">
+    {{ text }}
+  </button>
+</template>
+
+<style scoped>
+.sm {
+  padding: 0.5rem 1rem;
+  font-size: 0.875rem;
+}
+
+.lg {
+  padding: 0.875rem 2rem;
+  font-size: 1.5rem;
+}
+</style>
