16b5cbec65c80f26684b49b85dcfffd6cc9e1913
Adrian Kyzer <adriankyzer@gmail.com>
2024-09-23 13:42:52 +0200

    create a modal component

---
+++ src/components/ModalComponent.vue
@@ -0,0 +1,64 @@
+<script type="module" lang="ts">
+import { useModalStore } from '../store/useModalStore';
+import ButtonComponent from './ButtonComponent.vue';
+
+export default {
+  components: {
+    ButtonComponent
+  },
+  setup() {
+    const { state, closeModal } = useModalStore(); // Import the modal store
+    const buttonClickHandler = (type: String) => {
+      console.log('🚀 ~ buttonClickHandler ~ type:', type);
+      if (type === 'primary') {
+        state.buttonAction();
+      } else {
+        state.buttonActionSecondary();
+      }
+
+      console.log('🚀 ~ buttonClickHandler ~ state.closeOnAction:', state.closeOnAction);
+      if (state.closeOnAction) {
+        console.log('🚀 ~ buttonClickHandler ~ closeModal');
+        closeModal();
+      }
+    };
+
+    return {
+      state,
+      closeModal,
+      buttonClickHandler
+    };
+  }
+};
+</script>
+
+<template>
+  <div
+    v-if="state.showModal"
+    class="fixed inset-0 flex items-center justify-center bg-gray-900 bg-opacity-50 z-50"
+  >
+    <div class="bg-white p-6 rounded-lg shadow-lg max-w-sm text-center">
+      <div class="flex justify-between">
+        <h2 class="text-[1.75rem] font-semibold mb-4 flex">{{ state.title }}</h2>
+        <span @click="closeModal()" class="text-lg cursor-pointer">X</span>
+      </div>
+      <p class="mb-6 section-paragraph">{{ state.message }}</p>
+      <div class="flex justify-end space-x-4">
+        <ButtonComponent
+          v-if="state.buttonTextSecondary"
+          @click="() => buttonClickHandler('secondary')"
+          :text="state.buttonTextSecondary"
+          class="mr-2"
+          theme="inverse"
+          size="sm"
+        />
+        <ButtonComponent
+          @click="() => buttonClickHandler('primary')"
+          :text="state.buttonText"
+          class="ml-2"
+          size="sm"
+        />
+      </div>
+    </div>
+  </div>
+</template>
