127d88895b4d01bd1d911481d5942455c99db830
Adrian Kyzer <adriankyzer@gmail.com>
2024-09-24 16:55:56 +0200

    have menu bar fixed on mobile

--- src/views/ContentView.vue
+++ src/views/ContentView.vue
@@ -23,11 +23,11 @@ export default defineComponent({
 
 <template>
   <div class="min-h-screen">
-    <div class="h-full lg:w-[420px] lg:bg-transparent">
+    <div class="h-full lg:w-[420px]">
       <HeaderComponent />
     </div>
     <div
-      class="w-auto min-h-[calc(100vh-80px)] bg-[var(--color-background)] p-3 overflow-y-auto lg:h-full lg:p-16 lg:ml-[420px] lg:flex lg:justify-center lg:items-center xl:p-32"
+      class="w-auto mt-[80px] lg:mt-0 min-h-[calc(100vh-80px)] lg:min-h-[100vh] bg-[var(--color-background)] p-3 overflow-y-auto lg:h-full lg:p-16 lg:ml-[420px] lg:flex lg:justify-center lg:items-center xl:p-32"
     >
       <router-view></router-view>
     </div>
